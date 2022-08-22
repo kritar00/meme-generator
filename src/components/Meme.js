8d0a7018b3bc7e3f63fad89b48a659be0e503db8
kritar00 <91244044+kritar00@users.noreply.github.com>
2022-08-22 10:25:47 +0700

    second coming

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("");
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+
+    }
+
     return (
         <main className="meme--wrapper">
             <form>
@@ -8,8 +19,9 @@ export default function Meme() {
                     <input type={"text"} id="header" name="header" placeholder="text in here" />
                     <input type={"text"} id="footer" name="footer" placeholder="text in here" />
                 </div>
-                <button type="button">Get a new meme image</button>
+                <button onClick={getMemeImage} type="button">Get a new meme image</button>
             </form>
+                <img style={memeImage ? {display:"block"} : {display:"none"}} src={memeImage} className="meme--image" alt="just-meme" />
         </main>
-    )
+    )   
 }
